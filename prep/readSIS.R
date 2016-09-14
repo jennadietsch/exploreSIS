e8dcea41a21e507dcdcb1809c4964383e36bcb16
Joshh <josh.hagedorn@gmail.com>
2016-09-14 09:47:55 -0400

    Revert "Change LivType NAs to factor"
    This reverts commit 8eb1646dc0982553ee866c7736d44f6ac5d9245b.

--- prep/readSIS.R
+++ prep/readSIS.R
@@ -158,7 +158,7 @@
                                  'Privateresidencewithfamily' = 'Family';
                                  'Privateresidencewithfamilymembers' = 'Family';
                                  'SpecializedresidentialAFC' = 'Facility';
-                                 '' = 'Not Provided'")
+                                 '' = NA")
     
     sub_sis$LivingType <- as.factor(sub_sis$LivingType)
 
